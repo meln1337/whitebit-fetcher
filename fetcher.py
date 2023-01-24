9cd7b89f4624d1f905388e410f227dfa98d0af39
meln1337 <melnicukboris@gmail.com>
2023-01-25 00:45:40 +0200

    double

--- fetcher.py
+++ fetcher.py
@@ -25,10 +25,6 @@ exchange = "whitebit"
 res = requests.get("https://whitebit.com/api/v4/public/ticker")
 res = res.json()
 symbols = list(res.keys())
-print(symbols)
-print(len(symbols))
-# print("ZRX_USDT" in symbols)
-# symbols.remove("ZRX_USDT")
 
 trades_request = json.dumps({
     "id": 9,
@@ -78,8 +74,6 @@ elif mode == "orderbooks":
         ws_orderbooks.send(orderbooks_request)
         # print(f"Subscribed to {symbol}")
 
-    i = 0
-
     while True:
         try:
             data = ws_orderbooks.recv()
@@ -100,6 +94,65 @@ elif mode == "orderbooks":
 
         except:
             pass
-        i += 1
-        print(i)
 else:
+    while True:
+        ws_trades = websocket.create_connection("wss://api.whitebit.com/ws")
+        ws_trades.send(trades_request)
+
+        for i in range(50000):
+            try:
+                data = ws_trades.recv()
+                data = json.loads(data)
+                params = data["params"]
+                symbol = params[0]
+                symbol = symbol.replace("_", "-")
+                trades = params[1]
+                for trade in trades:
+                    ts = current_milli_time()
+                    price = trade["price"]
+                    amount = trade["amount"]
+                    side = "B" if trade["type"] == "buy" else "S"
+                    print(f"! {ts} {exchange} {symbol} {side} {price} {amount}")
+            except:
+                pass
+
+        ws_trades.close()
+
+        ws_orderbooks = websocket.create_connection("wss://api.whitebit.com/ws")
+
+        for symbol in symbols:
+            orderbooks_request = json.dumps({
+                "id": 12,
+                "method": "depth_subscribe",
+                "params": [
+                    symbol,
+                    100,
+                    "0",
+                    True
+                ]
+            })
+            ws_orderbooks.send(orderbooks_request)
+            # print(f"Subscribed to {symbol}")
+
+        for i in range(50000):
+            try:
+                data = ws_orderbooks.recv()
+                # print(data)
+                # time.sleep(0.1)
+                data = json.loads(data)
+                params = data["params"]
+                full_reload = params[0]
+                symbol = params[2]
+                symbol = symbol.replace("_", "-")
+                asks = params[1]["asks"]
+                ts = current_milli_time()
+                pq = "|".join([f"{ask[0]}@{ask[1]}" for ask in asks])
+                print(f"$ {ts} {exchange} {symbol} S {pq} {'R' if full_reload else ''}")
+                bids = params[1]["bids"]
+                pq = "|".join([f"{bid[0]}@{bid[1]}" for bid in bids])
+                print(f"$ {ts} {exchange} {symbol} B {pq} {'R' if full_reload else ''}")
+
+            except:
+                pass
+
+        ws_orderbooks.close()
