6006c6faa1e9105712ccb80a520b88d302287353
meln1337 <melnicukboris@gmail.com>
2023-01-25 01:00:23 +0200

    double using functions

--- fetcher.py
+++ fetcher.py
@@ -35,71 +35,29 @@ trades_request = json.dumps({
     ]
 })
 
-
-if mode == "trades":
+def trades():
     ws_trades = websocket.create_connection("wss://api.whitebit.com/ws")
     ws_trades.send(trades_request)
 
-    while True:
-        try:
-            data = ws_trades.recv()
-            data = json.loads(data)
-            params = data["params"]
-            symbol = params[0]
-            symbol = symbol.replace("_", "-")
-            trades = params[1]
-            for trade in trades:
-                ts = current_milli_time()
-                price = trade["price"]
-                amount = trade["amount"]
-                side = "B" if trade["type"] == "buy" else "S"
-                print(f"! {ts} {exchange} {symbol} {side} {price} {amount}")
-        except:
-            pass
-
-elif mode == "orderbooks":
-    ws_orderbooks = websocket.create_connection("wss://api.whitebit.com/ws")
-
-    for symbol in symbols:
-        orderbooks_request = json.dumps({
-            "id": 12,
-            "method": "depth_subscribe",
-            "params": [
-                symbol,
-                100,
-                "0",
-                True
-            ]
-        })
-        ws_orderbooks.send(orderbooks_request)
-        # print(f"Subscribed to {symbol}")
-
-    while True:
-        try:
-            data = ws_orderbooks.recv()
-            # print(data)
-            # time.sleep(0.1)
-            data = json.loads(data)
-            params = data["params"]
-            full_reload = params[0]
-            symbol = params[2]
-            symbol = symbol.replace("_", "-")
-            asks = params[1]["asks"]
-            ts = current_milli_time()
-            pq = "|".join([f"{ask[0]}@{ask[1]}" for ask in asks])
-            print(f"$ {ts} {exchange} {symbol} S {pq} {'R' if full_reload else ''}")
-            bids = params[1]["bids"]
-            pq = "|".join([f"{bid[0]}@{bid[1]}" for bid in bids])
-            print(f"$ {ts} {exchange} {symbol} B {pq} {'R' if full_reload else ''}")
-
-        except:
-            pass
-else:
-    while True:
-        ws_trades = websocket.create_connection("wss://api.whitebit.com/ws")
-        ws_trades.send(trades_request)
-
-        for i in range(50000):
+    if mode == "double":
+        for i in range(1000):
+            try:
+                data = ws_trades.recv()
+                data = json.loads(data)
+                params = data["params"]
+                symbol = params[0]
+                symbol = symbol.replace("_", "-")
+                trades = params[1]
+                for trade in trades:
+                    ts = current_milli_time()
+                    price = trade["price"]
+                    amount = trade["amount"]
+                    side = "B" if trade["type"] == "buy" else "S"
+                    print(f"! {ts} {exchange} {symbol} {side} {price} {amount}")
+            except:
+                pass
+    else:
+        while True:
             try:
                 data = ws_trades.recv()
                 data = json.loads(data)
@@ -116,25 +74,47 @@ else:
             except:
                 pass
 
-        ws_trades.close()
+    ws_trades.close()
+def orderbooks():
+    ws_orderbooks = websocket.create_connection("wss://api.whitebit.com/ws")
 
-        ws_orderbooks = websocket.create_connection("wss://api.whitebit.com/ws")
+    for symbol in symbols:
+        orderbooks_request = json.dumps({
+            "id": 12,
+            "method": "depth_subscribe",
+            "params": [
+                symbol,
+                100,
+                "0",
+                True
+            ]
+        })
+        ws_orderbooks.send(orderbooks_request)
+        # print(f"Subscribed to {symbol}")
 
-        for symbol in symbols:
-            orderbooks_request = json.dumps({
-                "id": 12,
-                "method": "depth_subscribe",
-                "params": [
-                    symbol,
-                    100,
-                    "0",
-                    True
-                ]
-            })
-            ws_orderbooks.send(orderbooks_request)
-            # print(f"Subscribed to {symbol}")
+    if mode == "double":
+        for i in range(5000):
+            try:
+                data = ws_orderbooks.recv()
+                # print(data)
+                # time.sleep(0.1)
+                data = json.loads(data)
+                params = data["params"]
+                full_reload = params[0]
+                symbol = params[2]
+                symbol = symbol.replace("_", "-")
+                asks = params[1]["asks"]
+                ts = current_milli_time()
+                pq = "|".join([f"{ask[0]}@{ask[1]}" for ask in asks])
+                print(f"$ {ts} {exchange} {symbol} S {pq} {'R' if full_reload else ''}")
+                bids = params[1]["bids"]
+                pq = "|".join([f"{bid[0]}@{bid[1]}" for bid in bids])
+                print(f"$ {ts} {exchange} {symbol} B {pq} {'R' if full_reload else ''}")
 
-        for i in range(50000):
+            except:
+                pass
+    else:
+        while True:
             try:
                 data = ws_orderbooks.recv()
                 # print(data)
@@ -155,4 +135,14 @@ else:
             except:
                 pass
 
-        ws_orderbooks.close()
+    ws_orderbooks.close()
+
+if mode == "trades":
+    trades()
+
+elif mode == "orderbooks":
+    orderbooks()
+else:
+    while True:
+        trades()
+        orderbooks()
